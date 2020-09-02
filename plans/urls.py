7c2356916078942f7d5259cea7a0cfdaddbae3e1
RonanMc <ronanmcclelland@gmail.com>
2020-09-02 13:52:36 +0100

    Created detail view for Sections

--- plans/urls.py
+++ plans/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_plans, plan_details
+from .views import list_plans, plan_details, section_details
 
 app_name = 'plans'
 
 urlpatterns = [
     path('', list_plans, name='plans-list'),
-    path('<slug>/', plan_details, name='plan-details'), 
+    path('<slug>/', plan_details, name='plan-details'),
+    path('<plan_slug>/<int:section_number>', section_details, name='section-details'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
-from .models import Plan
+from .models import Plan, Section
 
 def list_plans(request):
     queryset = Plan.objects.all()
@@ -15,3 +16,14 @@ def plan_details(request, slug):
         'plan': plan
     }
     return render(request, "plan-detail.html", context)
+
+def section_details(request, plan_slug, section_number):
+    section_qs = Section.objects \
+        .filter(plan__slug=plan_slug) \
+        .filter(section_number=section_number)
+    if section_qs.exists():
+        context = {
+            'section': section_qs[0],
+        }
+        return render(request, "section_detail.html", context)
+    return Http404
