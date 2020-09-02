0c179a504a1335d1642ce8eeeaa7019a9cb7529d
RonanMc <ronanmcclelland@gmail.com>
2020-09-02 11:25:19 +0100

    Create coach and plan models

--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Coach(models.Model):
+    ''' Coachs create the Plans '''
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Plan(models.Model):
+    ''' Plans are available for purchase '''
+    title = models.CharField(max_length=150)
+    date = models.DateTimeField()
+    slug = models.SlugField()
+    image = models.ImageField()
+    price = models.FloatField()
+    plan_id = models.CharField(max_length=10)
+    created_by = models.ManyToManyField(Coach)
+
+    def __str__(self):
+        return self.title
