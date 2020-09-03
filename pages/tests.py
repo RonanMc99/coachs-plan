ea658afbd0f7afd8847b6e4681c364445e7a563c
RonanMc <ronanmcclelland@gmail.com>
2020-09-03 14:58:15 +0100

    Added a test for the signup page

--- pages/tests.py
+++ pages/tests.py
@@ -1,9 +1,10 @@
-from django.test import SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
 
 
 class HomepageTests(SimpleTestCase):
-
+    ''' Test the home page '''
     def test_homepage_status(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -13,11 +14,30 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200) 
 
 class ContactpageTests(SimpleTestCase):
-
+    ''' Test the contact page '''
     def test_contactpage_status(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_contactpage_url_name(self):
         response = self.client.get(reverse('pages:contact'))
-        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(response.status_code, 200)
+
+
+class SignuppageTests(TestCase):
+    ''' Test the signup page '''
+    email = 'rontest@blah.com'
+    username = 'rontest'
+
+    def setUp(self):
+        url = reverse('account_signup')
+        self.response = self.client.get(url)
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()
+                        [0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()
+                        [0].email, self.email)
