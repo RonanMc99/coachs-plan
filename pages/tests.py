06065ea185cfce6adb25e8c80416e3918132e4b0
RonanMc <ronanmcclelland@gmail.com>
2020-09-03 14:14:35 +0100

    Added some simple unit tests for home and contact

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+
+    def test_homepage_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('pages:home'))
+        self.assertEqual(response.status_code, 200) 
+
+class ContactpageTests(SimpleTestCase):
+
+    def test_contactpage_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contactpage_url_name(self):
+        response = self.client.get(reverse('pages:contact'))
+        self.assertEqual(response.status_code, 200) 
