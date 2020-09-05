66d8f6f97e650b22db8f43dd80a4d5ebe747b7fc
RonanMc <ronanmcclelland@gmail.com>
2020-09-05 15:01:44 +0100

    Fixed additional allowed host setting

--- coachs_plan/settings.py
+++ coachs_plan/settings.py
@@ -183,8 +183,7 @@ if DEBUG is False:
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    ALLOWED_HOSTS = [*]
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 
