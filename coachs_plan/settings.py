4d03fe4ed401b60f1b160d1035a4e64138fb9026
RonanMc <ronanmcclelland@gmail.com>
2020-09-05 14:48:25 +0100

    Update DB settings for Postgres

--- coachs_plan/settings.py
+++ coachs_plan/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'coachs_plan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
-
 
 
 # Password validation
@@ -176,16 +186,6 @@ if DEBUG is False:
     ALLOWED_HOSTS = [*]
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
-    }
-}
 
 
 
