07ef492348f92809f5f8ffdc512f18b791fd4e07
RonanMc <ronanmcclelland@gmail.com>
2020-10-17 18:54:31 +0100

    Refactoring

--- plans/views.py
+++ plans/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Plan, Section, Activity
 from cart.models import Order, CartItem
 
+
 def list_plans(request):
     queryset = Plan.objects.all()
-    context = {
-        'queryset': queryset
-    }
+    context = {"queryset": queryset}
     return render(request, "plan-list.html", context)
+
 
 @login_required
 def plan_details(request, slug):
@@ -24,33 +24,33 @@ def plan_details(request, slug):
             order_item = order_item_qs[0]
             if order_item in order.items.all():
                 plan_in_cart = True
-    context = {
-        'plan': plan,
-        'in_cart_item': plan_in_cart
-    }
+    context = {"plan": plan, "in_cart_item": plan_in_cart}
     return render(request, "plan-detail.html", context)
+
 
 @login_required
 def section_details(request, plan_slug, section_number):
-    section_qs = Section.objects \
-        .filter(plan__slug=plan_slug) \
-        .filter(section_number=section_number)
+    section_qs = Section.objects.filter(plan__slug=plan_slug).filter(
+        section_number=section_number
+    )
     if section_qs.exists():
         context = {
-            'section': section_qs[0],
+            "section": section_qs[0],
         }
         return render(request, "section_detail.html", context)
     return Http404
 
+
 @login_required
 def activity_details(request, plan_slug, section_number, activity_number):
-    activity_qs = Activity.objects \
-        .filter(section__plan__slug=plan_slug) \
-        .filter(section__section_number=section_number) \
+    activity_qs = (
+        Activity.objects.filter(section__plan__slug=plan_slug)
+        .filter(section__section_number=section_number)
         .filter(activity_number=activity_number)
+    )
     if activity_qs.exists():
         context = {
-            'activity': activity_qs[0],
+            "activity": activity_qs[0],
         }
         return render(request, "activity_detail.html", context)
     return Http404
