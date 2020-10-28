413fcbeb0a4d7230fec3e473056ed409e18313cd
RonanMc <ronanmcclelland@gmail.com>
2020-10-28 19:38:06 +0000

    Added blog model and updated coach model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+from plans import Coach
+
+
+class Post(models.Model):
+    """ A list of plans that the user has access to """
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(default=timezone.now)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(Coach, null=True, blank=True, on_delete=models.SET_NULL)
--- plans/models.py
+++ plans/models.py
@@ -36,6 +36,7 @@ class Coach(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     slug = models.SlugField(unique=True)
+    blurb = models.TextField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
