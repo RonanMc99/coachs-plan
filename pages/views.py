5fffdd795be0bbaa81b7dd13fb012be0d02ae3f4
RonanMc <ronanmcclelland@gmail.com>
2020-10-31 14:29:40 +0000

    Updated the user's order history / profile view

--- pages/views.py
+++ pages/views.py
@@ -1,34 +1,37 @@
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import reverse, render
 from django.views.generic import TemplateView, FormView
 from .forms import ContactForm
-from cart.models import Order
+from cart.models import CompletedOrder
 
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
 
 class ContactPageView(FormView):
     form_class = ContactForm
-    template_name = 'contact.html'
+    template_name = "contact.html"
 
     def get_success_url(self):
-        return reverse('pages:contact')
-    
+        return reverse("pages:contact")
+
     def form_valid(self, form):
-        messages.info(self.request, "Thanks for reaching out. We have received your message")
-        name = form.cleaned_data.get('name')
-        email = form.cleaned_data.get('email')
-        message = form.cleaned_data.get('message')
-        message_instance = 'Contact form message: "Name:{name}", "Email:{email}", "Message:{message}"'      
+        messages.info(
+            self.request, "Thanks for reaching out. We have received your message"
+        )
+        name = form.cleaned_data.get("name")
+        email = form.cleaned_data.get("email")
+        message = form.cleaned_data.get("message")
+        message_instance = (
+            'Contact form message: "Name:{name}", "Email:{email}", "Message:{message}"'
+        )
         return super(ContactPageView, self).form_valid(form)
 
 
 def profile_view(request):
-    orders = Order.objects.filter(user=request.user, purchased=True)
-    context = {
-        'orders': orders
-    }
-    
+    user = request.user
+    orders = CompletedOrder.objects.filter(order__user=user)
+    context = {"orders": orders}
     return render(request, "user_profile.html", context)
