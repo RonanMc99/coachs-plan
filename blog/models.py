c3a36557ad6c4ead77e4d9dd3e42b1f8badadd22
RonanMc <ronanmcclelland@gmail.com>
2020-10-31 12:46:04 +0000

    Added field for filtering status

--- blog/models.py
+++ blog/models.py
@@ -7,16 +7,18 @@ from plans.models import Coach
 
 class Post(models.Model):
     """ A list of plans that the user has access to """
+
     STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
+        ("draft", "Draft"),
+        ("published", "Published"),
     )
     title = models.CharField(max_length=150)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     publish = models.DateTimeField(default=timezone.now)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(Coach, null=True, blank=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
 
     def __str__(self):
         return self.title
