22778febb8745f2b4dcc670dff953babb9b29f77
RonanMc <ronanmcclelland@gmail.com>
2020-11-04 20:45:36 +0000

    Update allowed_hosts for production

--- coachs_plan/settings.py
+++ coachs_plan/settings.py
@@ -16,7 +16,7 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', default='prod')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
