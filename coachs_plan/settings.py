712736059faa873dc572f682ad4413738789ee7c
RonanMc <ronanmcclelland@gmail.com>
2020-11-05 18:47:22 +0000

    Set the DB as Postgres for Docker deployment

--- coachs_plan/settings.py
+++ coachs_plan/settings.py
@@ -78,25 +78,25 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 WSGI_APPLICATION = "coachs_plan.wsgi.application"
 
 # Databases
-if ENVIRONMENT != 'prod':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if ENVIRONMENT != 'prod':
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "postgres",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "db",
-            "PORT": 5432,
-        }
+# else:
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
+}
 
 # Auth
 AUTH_PASSWORD_VALIDATORS = [
